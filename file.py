e74a3cac20422788bc1971158b357d85cfa6023e
sanketsatyam7 <55615027+sanketsatyam7@users.noreply.github.com>
2019-09-21 03:31:38 -0400

    dummy

---
+++ file.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+print("Soumya")
+
+x=9+6
+y=5+9
+print(x+y)
+
+A=4
+A="Soumya"
+print (A)
+
+a="This is"
+b=" Soumya"
+c=a+b
+print(c[3])
+
+print(type(b))
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = " Hello, World! "
+print(a.replace("o","X")) 
